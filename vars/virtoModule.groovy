97f7f07d4fad151b471f2eb457c677c26e0b80a5
Sasha <Woland2k@users.noreply.github.com>
2016-06-10 15:40:21 -0700

    Update virtoModule.groovy

--- vars/virtoModule.groovy
+++ vars/virtoModule.groovy
@@ -8,4 +8,20 @@ def call(body) {
     
     // you can call any valid step functions from your code, just like you can from Pipeline scripts
     echo "Building plugin ${config.name}"
+    
+    if(env.BRANCH_NAME=="master"){
+        stage name: 'Deploy to Prod', concurrency: 1
+            updateVersion(pwd())
+    }
 }
+
+def updateVersion(workspace)
+{
+    bat "powershell.exe -File \"${env.VC_RES}\\script\\version3.ps1\" -solutiondir \"${workspace}\""
+    bat "\"${tool 'Git'}\" config user.email \"ci@virtocommerce.com\""
+    bat "\"${tool 'Git'}\" config user.name \"Virto Jenkins\""
+    bat "\"${tool 'Git'}\" commit -am \"Updated version number\""
+    bat "\"${tool 'Git'}\" push origin HEAD:master -f"
+}
+
+
