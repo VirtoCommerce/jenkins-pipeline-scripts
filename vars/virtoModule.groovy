62db65753edfbdd432c743b9c0eb445b0d01f985
Sasha <Woland2k@users.noreply.github.com>
2016-06-14 17:30:11 -0700

    Update virtoModule.groovy

--- vars/virtoModule.groovy
+++ vars/virtoModule.groovy
@@ -13,7 +13,8 @@ def call(body) {
     
 	env.WORKSPACE = pwd()
 	stage 'Checkout'
-		checkout([$class: 'GitSCM', branches: [[name: '${env.BRANCH_NAME}']], locations: [[clean: false, FilePath: '.']], extensions: [[$class: 'PathRestriction', excludedRegions: 'CommonAssemblyInfo\\.cs', includedRegions: '']]])
+		checkout scm
+		//checkout([$class: 'GitSCM', branches: [[name: '${env.BRANCH_NAME}']], locations: [[clean: false, FilePath: '.']], extensions: [[$class: 'PathRestriction', excludedRegions: 'CommonAssemblyInfo\\.cs', includedRegions: '']]])
 
 		stage 'Build'
 			bat "Nuget restore ${solution}"
