c478bcf53b2ee42a4ac489c348637fd6d0d9464a
Sasha <Woland2k@users.noreply.github.com>
2016-06-18 15:07:13 -0700

    Update and rename virtoModule.groovy to virtoModuleOld.groovy

--- vars/virtoModuleOld.groovy
+++ vars/virtoModuleOld.groovy
@@ -27,7 +27,7 @@ def call(body) {
 
 	env.WORKSPACE = pwd()
 	stage 'Checkout'
-		checkout scm
+		
 		//checkout([$class: 'GitSCM', branches: [[name: "*/${env.BRANCH_NAME}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: ''], [$class: 'CheckoutOption']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'sasha-jenkins', url: "git@github.com:VirtoCommerce/${repo}.git"]]])
 		//checkout([$class: 'GitSCM', extensions: [[$class: 'PathRestriction', excludedRegions: 'CommonAssemblyInfo\\.cs']]])
 		/*
