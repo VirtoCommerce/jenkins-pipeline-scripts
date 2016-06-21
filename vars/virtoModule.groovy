036634e583be3b3ef3104f0996de47e486b8aacf
Sasha <Woland2k@users.noreply.github.com>
2016-06-21 14:16:49 -0700

    Update virtoModule.groovy

--- vars/virtoModule.groovy
+++ vars/virtoModule.groovy
@@ -12,25 +12,176 @@ def call(body) {
     {
     	try 
       	{
-      		// you can call any valid step functions from your code, just like you can from Pipeline scripts
-    		echo "Building branch ${env.BRANCH_NAME}, job: ${env.JOB_NAME}"
-		stage 'Checkout'
-  			checkout scm
-  		
-  		buildSolutions()
-  		runTests()
-  	
+      		stage 'Checkout'
+			checkout scm
+		
+		stage 'Build'
+			buildSolutions()
+		
+		stage 'Testing'
+			runTests()
+		
+		bat "\"${tool 'Git'}\" log -1 --pretty=%%B > LAST_COMMIT_MESSAGE"
+		git_last_commit=readFile('LAST_COMMIT_MESSAGE')
+	
+		if (env.BRANCH_NAME == 'master' && git_last_commit.contains('[publish]'))
+		{
+			stage 'Publishing'
+				processManifests()
+		}
       	}
 	catch (any) {
       		currentBuild.result = 'FAILURE'
       		throw any //rethrow exception to prevent the build from proceeding
 	} 
     	finally {
-      		step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: 'dev@virtoway.com', sendToIndividuals: true])
+      		step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: 'sasha@virtoway.com', sendToIndividuals: true])
     	}
 
   	step([$class: 'GitHubCommitStatusSetter', statusResultSource: [$class: 'ConditionalStatusResultSource', results: []]])
     }
+}
+
+def processManifests()
+{
+	// find all manifests
+	def manifests = findFiles(glob: '**\\module.manifest')
+		
+	if(manifests.size() > 0)
+	{
+		for(int i = 0; i < manifests.size(); i++)
+		{
+			def manifest = manifests[i]
+			processManifest(manifest.path)
+		}
+	}
+	else
+	{
+		echo "no module.manifest files found"
+	}
+}
+
+def processManifest(def manifestPath)
+{
+	echo "reading $manifestPath"
+	def manifestFile = readFile file: "$manifestPath", encoding: 'utf-8'
+	def manifest = new XmlSlurper().parseText(manifestFile)
+	manifestFile = null
+
+	//echo manifestFile
+    	echo "Upading module ${manifest.id}"
+    	def id = manifest.id.toString()
+    	
+    	def version = manifest.version.toString() 
+    	def platformVersion = manifest.platformVersion.toString()
+    	def title = manifest.title.toString()
+    	def description = manifest.description.toString()
+    	def projectUrl = manifest.projectUrl.toString()
+    	def packageUrl = manifest.packageUrl.toString()
+    	def iconUrl = manifest.iconUrl.toString()
+    	manifest = null
+    	
+    	def manifestDirectory = manifestPath.substring(0, manifestPath.length() - 16)
+    	packageUrl = publishRelease(manifestDirectory, version)
+    	
+    	updateModule(
+    		id, 
+    		version, 
+    		platformVersion,
+    		title,
+    		description,
+    		projectUrl,
+    		packageUrl,
+    		iconUrl)
+}
+
+def updateModule(def id, def version, def platformVersion, def title, def description, def projectUrl, def packageUrl, def iconUrl)
+{
+	// MODULES
+        dir('modules') {
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'sasha-jenkins', url: 'git@github.com:VirtoCommerce/vc-modules.git']]])
+
+            def inputFile = readFile file: 'modules.json', encoding: 'utf-8'
+            def parser = new JsonSlurper()
+            def json = parser.parseText(inputFile)
+            def builder = new JsonBuilder(json)
+            
+            for (rec in json) {
+               if ( rec.id == id) {
+               	    rec.description = description
+               	    rec.title = title
+               	    rec.description = description
+               	    if (projectUrl!=null && projectUrl.length()>0)
+               	    {
+               	    	rec.projectUrl = projectUrl
+               	    }
+               	    if (packageUrl!=null && packageUrl.length()>0)
+               	    {
+               	    	rec.packageUrl = packageUrl
+               	    }
+               	    if (iconUrl!=null && iconUrl.length()>0)
+               	    {
+               	    	rec.iconUrl = iconUrl
+               	    }
+		break
+               }
+            }
+            
+            println(builder.toString())
+        }
+}
+
+def publishRelease(def manifestDirectory, def version)
+{
+	def REPO_ORG = "VirtoCommerce"
+	def REPO_NAME = "vc-module-jenkinssample"
+	def tempFolder = pwd(tmp: true)
+	def wsFolder = pwd()
+	def tempDir = "$tempFolder\\vc-module"
+	def modulesDir = "$tempDir\\_PublishedWebsites"
+	def packagesDir = "$wsFolder\\artifacts"
+	def foundProjects = false
+		    		
+	dir(packagesDir)
+	{
+		deleteDir()
+	}
+			
+	dir(manifestDirectory)
+	{
+		def projects = findFiles(glob: '*.csproj')
+		if(projects.size() > 0)
+		{
+			for(int i = 0; i < projects.size(); i++)
+			{
+				def project = projects[i]
+				bat "\"${tool 'MSBuild 12.0'}\" \"$project.name\" /nologo /verbosity:m /t:PackModule /p:Configuration=Release /p:Platform=\"Any CPU\" /p:DebugType=none /p:AllowedReferenceRelatedFileExtensions=: \"/p:OutputPath=$tempDir\" \"/p:VCModulesOutputDir=$modulesDir\" \"/p:VCModulesZipDir=$packagesDir\""			
+			}
+			
+			foundProjects = true
+		}
+	}
+
+	if(foundProjects)
+	{
+		dir(packagesDir)
+		{
+			def artifacts = findFiles(glob: '*.zip')
+			if(artifacts.size() > 0)
+			{
+				for(int i = 0; i < artifacts.size(); i++)
+				{
+					def artifact = artifacts[i]
+					bat "${env.Utils}\\github-release release --user $REPO_ORG --repo $REPO_NAME --tag v${version}"
+					bat "${env.Utils}\\github-release upload --user $REPO_ORG --repo $REPO_NAME --tag v${version} --name \"${artifact}\" --file \"${artifact}\""
+					echo "uploaded to https://github.com/$REPO_ORG/$REPO_NAME/releases/download/v${version}/${artifact}"
+					return  "https://github.com/$REPO_ORG/$REPO_NAME/releases/download/v${version}/${artifact}"
+				}
+			}
+		}
+	}
+
+	//bat "${env.Utils}\\github-release info -u VirtoCommerce -r vc-module-jenkinssample"
 }
 
 def buildSolutions()
