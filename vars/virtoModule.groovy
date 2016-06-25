1891fcf68fc1c5d9d4b6c17a5389edb4fa28b6ba
Sasha <Woland2k@users.noreply.github.com>
2016-06-24 17:29:25 -0700

    Update virtoModule.groovy

--- vars/virtoModule.groovy
+++ vars/virtoModule.groovy
@@ -307,7 +307,7 @@ def runTests()
 				paths += "\"$testDll.path\" "
 			}
 			
-			bat "${xUnitExecutable} ${paths} -xml xUnit.Test.xml -trait 'category=ci' -parallel none"
+			bat "${xUnitExecutable} ${paths} -xml xUnit.Test.xml -trait \"category=ci\" -parallel none"
 			step([$class: 'XUnitPublisher', testTimeMargin: '3000', thresholdMode: 1, thresholds: [[$class: 'FailedThreshold', failureNewThreshold: '', failureThreshold: '', unstableNewThreshold: '', unstableThreshold: ''], [$class: 'SkippedThreshold', failureNewThreshold: '', failureThreshold: '', unstableNewThreshold: '', unstableThreshold: '']], tools: [[$class: 'XUnitDotNetTestType', deleteOutputFiles: true, failIfNotNew: false, pattern: '*.xml', skipNoTestFiles: true, stopProcessingIfError: false]]])
 	}
 }
