9a9290336b91844e385a7508846917c8fe5a79b8
Sasha <Woland2k@users.noreply.github.com>
2016-06-24 17:05:14 -0700

    Update virtoPlatform.groovy

--- vars/virtoPlatform.groovy
+++ vars/virtoPlatform.groovy
@@ -2,17 +2,18 @@
 
 // module script
 def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+	// evaluate the body block, and collect configuration into the object
+	def config = [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config
+	body()
     
 	node
 	{
-		def solution = 'VirtoCommerce.Platform.sln'
-		try 
-		    {
+		def solution = config.solution
+		if(solution == null)
+			 solution = 'VirtoCommerce.Platform.sln'
+		try {
 	    		// you can call any valid step functions from your code, just like you can from Pipeline scripts
 			echo "Building branch ${env.BRANCH_NAME}"
 			stage 'Checkout'
@@ -22,13 +23,13 @@ def call(body) {
 				bat "\"${tool 'MSBuild 12.0'}\" \"${solution}\" /p:Configuration=Debug /p:Platform=\"Any CPU\""
 		
 		    	runTests()
-		    }
+		}
 		catch (any) {
 			currentBuild.result = 'FAILURE'
 			throw any //rethrow exception to prevent the build from proceeding
 		}
 		finally {
-	    		step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: 'dev@virtoway.com', sendToIndividuals: true])
+	    		step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: 'sasha@virtoway.com', sendToIndividuals: true])
 		}
 	
 	  	step([$class: 'GitHubCommitStatusSetter', statusResultSource: [$class: 'ConditionalStatusResultSource', results: []]])
