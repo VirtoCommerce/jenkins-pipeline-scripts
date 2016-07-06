d9bcb1037408d9d0f438eae7089037e6d4b8e30b
Sasha <Woland2k@users.noreply.github.com>
2016-07-05 21:57:33 -0700

    Update twitter.ps1

--- vars/twitter.ps1
+++ vars/twitter.ps1
@@ -8,5 +8,10 @@ Param(
      )
 
 Import-Module "$PSScriptRoot\InvokeTwitterAPIs.psm1"
-$OAuth = @{'ApiKey' = '$env:twitter_apikey'; 'ApiSecret' = '$env:twitter_apisecret';'AccessToken' = '$env:twitter_oauth_token';'AccessTokenSecret' = '$env:twitter_oauth_token_secret'}
+$apikey = "$env:twitter_apikey"
+$apisecret = "$env:twitter_apisecret"
+$access_token = "$env:twitter_oauth_token"
+$access_token_secret = "$env:twitter_oauth_token_secret"
+
+$OAuth = @{'ApiKey' = $apikey; 'ApiSecret' = $apisecret;'AccessToken' = $access_token;'AccessTokenSecret' = $access_token_secret}
 Invoke-TwitterRestMethod -ResourceURL 'https://api.twitter.com/1.1/statuses/update.json' -RestVerb 'POST' -Parameters @{'status'=$status} -OAuthSettings $OAuth
