ac193a9e58442c5e9034ce9579d1c26dfed242f5
Sasha <sasha@virtoway.com>
2016-07-23 12:25:08 -0700

    update module

--- vars/virtoModule.groovy
+++ vars/virtoModule.groovy
@@ -2,46 +2,44 @@
 import groovy.json.*
 import groovy.util.*
 
-// module script
-def call(body) {
+	// module script
+	def call(body) {
     // evaluate the body block, and collect configuration into the object
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    
+
     node
     {
-    	try 
-      	{
-      		stage 'Checkout'
+		try {
+			stage 'Checkout'
 			checkout scm
 		
-		stage 'Build'
+			stage 'Build'
 			buildSolutions()
 			processManifests(false) // prepare artifacts for testing
-		
-		stage 'Testing'
-			runTests()
-		
-		bat "\"${tool 'Git'}\" log -1 --pretty=%%B > LAST_COMMIT_MESSAGE"
-		git_last_commit=readFile('LAST_COMMIT_MESSAGE')
-	
-		if (env.BRANCH_NAME == 'master' && git_last_commit.contains('[publish]'))
-		{
-			stage 'Publishing'
-				processManifests(true) // publish artifacts to github releases
-		}
-      	}
-	catch (any) {
-      		currentBuild.result = 'FAILURE'
-      		throw any //rethrow exception to prevent the build from proceeding
-	} 
-    	finally {
-      		step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: 'dev@virtoway.com', sendToIndividuals: true])
-    	}
 
-  	step([$class: 'GitHubCommitStatusSetter', statusResultSource: [$class: 'ConditionalStatusResultSource', results: []]])
+			stage 'Testing'
+			runTests()
+
+			bat "\"${tool 'Git'}\" log -1 --pretty=%%B > LAST_COMMIT_MESSAGE"
+			git_last_commit = readFile('LAST_COMMIT_MESSAGE')
+
+			if (env.BRANCH_NAME == 'master' && git_last_commit.contains('[publish]')) {
+				stage 'Publishing'
+				processManifests(true) // publish artifacts to github releases
+			}
+		}
+		catch (any) {
+			currentBuild.result = 'FAILURE'
+			throw any //rethrow exception to prevent the build from proceeding
+		}
+		finally {
+			step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: 'dev@virtoway.com', sendToIndividuals: true])
+		}
+
+		step([$class: 'GitHubCommitStatusSetter', statusResultSource: [$class: 'ConditionalStatusResultSource', results: []]])
     }
 }
 
@@ -49,17 +47,15 @@ def processManifests(publish)
 {
 	// find all manifests
 	def manifests = findFiles(glob: '**\\module.manifest')
-		
-	if(manifests.size() > 0)
-	{
-		for(int i = 0; i < manifests.size(); i++)
+
+	if (manifests.size() > 0) {
+		for (int i = 0; i < manifests.size(); i++)
 		{
 			def manifest = manifests[i]
 			processManifest(publish, manifest.path)
 		}
 	}
-	else
-	{
+	else {
 		echo "no module.manifest files found"
 	}
 }
@@ -72,64 +68,63 @@ def processManifest(def publish, def manifestPath)
 	manifestFile = null
 
 	//echo manifestFile
-    	echo "Upading module ${manifest.id}"
-    	def id = manifest.id.toString()
-    	
-    	def version = manifest.version.toString() 
-    	def platformVersion = manifest.platformVersion.toString()
-    	def title = manifest.title.toString()
-    	def description = manifest.description.toString()
-    	def projectUrl = manifest.projectUrl.toString()
-    	def packageUrl = manifest.packageUrl.toString()
-    	def iconUrl = manifest.iconUrl.toString()
-    	
-    	// get dependencies
-    	def dependencies = []
-    	for(int i = 0; i < manifest.dependencies.dependency.size(); i++)
+	echo "Upading module ${manifest.id}"
+	def id = manifest.id.toString()
+
+	def version = manifest.version.toString()
+	def platformVersion = manifest.platformVersion.toString()
+	def title = manifest.title.toString()
+	def description = manifest.description.toString()
+	def projectUrl = manifest.projectUrl.toString()
+	def packageUrl = manifest.packageUrl.toString()
+	def iconUrl = manifest.iconUrl.toString()
+
+	// get dependencies
+	def dependencies = []
+	for (int i = 0; i < manifest.dependencies.dependency.size(); i++)
 	{
 		def dependency = manifest.dependencies.dependency[i]
 		def dependencyObj = [id: dependency['@id'].text(), version: dependency['@version'].text()]
 		dependencies.add(dependencyObj)
 	}
-	
+
 	def owners = []
-    	for(int i = 0; i < manifest.owners.owner.size(); i++)
+	for (int i = 0; i < manifest.owners.owner.size(); i++)
 	{
 		def owner = manifest.owners.owner[i]
 		owners.add(owner.text())
 	}
-	
+
 	def authors = []
-    	for(int i = 0; i < manifest.authors.author.size(); i++)
+	for (int i = 0; i < manifest.authors.author.size(); i++)
 	{
 		def author = manifest.authors.author[i]
 		authors.add(author.text())
 	}
-	
-    	manifest = null
-    	
-    	def manifestDirectory = manifestPath.substring(0, manifestPath.length() - 16)
-    	prepareRelease(manifestDirectory)
-    	
-    	if(publish)
-    	{
-	    	packageUrl = publishRelease(version)
-	    	
-	    	updateModule(
-	    		id, 
-	    		version, 
-	    		platformVersion,
-	    		title,
-	    		authors,
-	    		owners,
-	    		description,
-	    		dependencies,
-	    		projectUrl,
-	    		packageUrl,
-	    		iconUrl)
-	    		
-	    	publishTweet("${title} ${version} ${projectUrl} #virtocommerceci")
-    	}
+
+	manifest = null
+
+	def manifestDirectory = manifestPath.substring(0, manifestPath.length() - 16)
+	prepareRelease(manifestDirectory)
+
+	if (publish) {
+		packageUrl = publishRelease(version)
+
+		updateModule(
+			id,
+			version,
+			platformVersion,
+			title,
+			authors,
+			owners,
+			description,
+			dependencies,
+			projectUrl,
+			packageUrl,
+			iconUrl)
+
+		publishTweet("${title} ${version} ${projectUrl} #virtocommerceci")
+	}
 }
 
 def publishTweet(def status)
@@ -140,75 +135,71 @@ def publishTweet(def status)
 def updateModule(def id, def version, def platformVersion, def title, def authors, def owners, def description, def dependencies, def projectUrl, def packageUrl, def iconUrl)
 {
 	// MODULES
-        dir('modules') {
-            checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'sasha-jenkins', url: 'git@github.com:VirtoCommerce/vc-modules.git']]])
+	dir('modules') {
+		checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'sasha-jenkins', url: 'git@github.com:VirtoCommerce/vc-modules.git']]])
 
-            def inputFile = readFile file: 'modules.json', encoding: 'utf-8'
-            def parser = new JsonSlurper()
-            def json = parser.parseText(inputFile)
-            parser = null
-            def builder = new JsonBuilder(json)
-            
-           def foundRecord = false
-            
-            for (rec in json) {
-               if ( rec.id == id) {
+		def inputFile = readFile file: 'modules.json', encoding: 'utf-8'
+		def parser = new JsonSlurper()
+		def json = parser.parseText(inputFile)
+		parser = null
+		def builder = new JsonBuilder(json)
+
+		def foundRecord = false
+
+		for (rec in json) {
+			if (rec.id == id) {
                	echo "Modifying existing record in modules.json"
-               	    rec.description = description
-               	    rec.title = title
-               	    rec.version = version
-               	    rec.platformVersion = platformVersion
-               	    rec.description = description
-               	    rec.dependencies = dependencies
-               	    if (projectUrl!=null && projectUrl.length()>0)
-               	    {
-               	    	rec.projectUrl = projectUrl
-               	    }
-               	    if (packageUrl!=null && packageUrl.length()>0)
-               	    {
-               	    	rec.packageUrl = packageUrl
-               	    }
-               	    if (iconUrl!=null && iconUrl.length()>0)
-               	    {
-               	    	rec.iconUrl = iconUrl
-               	    }
-               	    
-               	    rec.dependencies = dependencies
-               	    rec.authors = authors
-               	    rec.owners = owners
-               	    
+				rec.description = description
+				rec.title = title
+				rec.version = version
+				rec.platformVersion = platformVersion
+				rec.description = description
+				rec.dependencies = dependencies
+				if (projectUrl != null && projectUrl.length() > 0) {
+					rec.projectUrl = projectUrl
+				}
+				if (packageUrl != null && packageUrl.length() > 0) {
+					rec.packageUrl = packageUrl
+				}
+				if (iconUrl != null && iconUrl.length() > 0) {
+					rec.iconUrl = iconUrl
+				}
+
+				rec.dependencies = dependencies
+				rec.authors = authors
+				rec.owners = owners
+
                 foundRecord = true
-		break
-               }
-            }
-            
-		if(!foundRecord)
-		{
-             	// create new
-		echo "Creating new record in modules.json"
-		json.add([
-			id: id, 
-			title: title, 
-			version: version,
-			platformVersion: platformVersion,
-			authors: authors,
-			owners: owners,
-			description: description, 
-			dependencies: dependencies, 
-			projectUrl: projectUrl, 
-			packageUrl: packageUrl,
-			iconUrl: iconUrl
+				break
+			}
+		}
+
+		if (!foundRecord) {
+			// create new
+			echo "Creating new record in modules.json"
+			json.add([
+				id: id,
+				title: title,
+				version: version,
+				platformVersion: platformVersion,
+				authors: authors,
+				owners: owners,
+				description: description,
+				dependencies: dependencies,
+				projectUrl: projectUrl,
+				packageUrl: packageUrl,
+				iconUrl: iconUrl
 			])
-	        }
-            
+		}
+
 		def moduleJson = builder.toString()
-            	builder = null
-            	def prettyModuleJson = JsonOutput.prettyPrint(moduleJson.toString())
-	        //println(moduleJson)
-	        writeFile file: 'modules.json', text: prettyModuleJson
-        }
-        
-        pushModules('modules', id)
+		builder = null
+		def prettyModuleJson = JsonOutput.prettyPrint(moduleJson.toString())
+		//println(moduleJson)
+		writeFile file: 'modules.json', text: prettyModuleJson
+	}
+
+	pushModules('modules', id)
 }
 
 def pushModules(def directory, def module)
@@ -227,8 +218,8 @@ def pushModules(def directory, def module)
 	    		bat "\"${tool 'Git'}\" commit -am \"Added new module ${id}\""
 	    	}
 	    	*/
-	    	bat "\"${tool 'Git'}\" commit -am \"Updated module ${module}\""
-	    	bat "\"${tool 'Git'}\" push origin HEAD:master -f"
+		bat "\"${tool 'Git'}\" commit -am \"Updated module ${module}\""
+		bat "\"${tool 'Git'}\" push origin HEAD:master -f"
 	}
 }
 
@@ -239,18 +230,17 @@ def prepareRelease(def manifestDirectory)
 	def tempDir = "$tempFolder\\vc-module"
 	def modulesDir = "$tempDir\\_PublishedWebsites"
 	def packagesDir = "$wsFolder\\artifacts"
-		    		
+
 	dir(packagesDir)
 	{
 		deleteDir()
 	}
-			
+
 	dir(manifestDirectory)
 	{
 		def projects = findFiles(glob: '*.csproj')
-		if(projects.size() > 0)
-		{
-			for(int i = 0; i < projects.size(); i++)
+		if (projects.size() > 0) {
+			for (int i = 0; i < projects.size(); i++)
 			{
 				def project = projects[i]
 				bat "\"${tool 'MSBuild 12.0'}\" \"$project.name\" /nologo /verbosity:m /t:PackModule /p:Configuration=Release /p:Platform=\"Any CPU\" /p:DebugType=none /p:AllowedReferenceRelatedFileExtensions=: \"/p:OutputPath=$tempDir\" \"/p:VCModulesOutputDir=$modulesDir\" \"/p:VCModulesZipDir=$packagesDir\""			
@@ -262,13 +252,13 @@ def prepareRelease(def manifestDirectory)
 def publishRelease(def version)
 {
 	tokens = "${env.JOB_NAME}".tokenize('/')
-    	def REPO_NAME = tokens[1]
+	def REPO_NAME = tokens[1]
 	def REPO_ORG = "VirtoCommerce"
 
 	def tempFolder = pwd(tmp: true)
 	def wsFolder = pwd()
 	def packagesDir = "$wsFolder\\artifacts"
-		    		
+
 	dir(packagesDir)
 	{
 		deleteDir()
@@ -277,15 +267,14 @@ def publishRelease(def version)
 	dir(packagesDir)
 	{
 		def artifacts = findFiles(glob: '*.zip')
-		if(artifacts.size() > 0)
-		{
-			for(int i = 0; i < artifacts.size(); i++)
+		if (artifacts.size() > 0) {
+			for (int i = 0; i < artifacts.size(); i++)
 			{
 				def artifact = artifacts[i]
 				bat "${env.Utils}\\github-release release --user $REPO_ORG --repo $REPO_NAME --tag v${version}"
 				bat "${env.Utils}\\github-release upload --user $REPO_ORG --repo $REPO_NAME --tag v${version} --name \"${artifact}\" --file \"${artifact}\""
 				echo "uploaded to https://github.com/$REPO_ORG/$REPO_NAME/releases/download/v${version}/${artifact}"
-				return  "https://github.com/$REPO_ORG/$REPO_NAME/releases/download/v${version}/${artifact}"
+				return "https://github.com/$REPO_ORG/$REPO_NAME/releases/download/v${version}/${artifact}"
 			}
 		}
 	}
@@ -295,9 +284,8 @@ def buildSolutions()
 {
 	def solutions = findFiles(glob: '*.sln')
 
-	if(solutions.size() > 0)
-	{
-		for(int i = 0; i < solutions.size(); i++)
+	if (solutions.size() > 0) {
+		for (int i = 0; i < solutions.size(); i++)
 		{
 			def solution = solutions[i]
 			bat "Nuget restore ${solution.name}"
@@ -310,22 +298,24 @@ def runTests()
 {
 	def xUnit = env.XUnit
 	def xUnitExecutable = "${xUnit}\\xunit.console.exe"
-	
+
 	def testDlls = findFiles(glob: '**\\bin\\Debug\\*Test.dll')
-	if(testDlls.size() > 0)
-	{
+	if (testDlls.size() > 0) {
 		stage 'Running tests'
-			String paths = ""
-			for(int i = 0; i < testDlls.size(); i++)
-			{
-				def testDll = testDlls[i]
-				paths += "\"$testDll.path\" "
-			}
-			
-			// add platform dll to test installs
-			paths += "\"..\\..\\..\\vc-platform\\dev\\workspace\\virtocommerce.platform.tests\\bin\\debug\\VirtoCommerce.Platform.Test.dll\""
-			
-			bat "${xUnitExecutable} ${paths} -xml xUnit.Test.xml -trait \"category=ci\" -parallel none"
-			step([$class: 'XUnitPublisher', testTimeMargin: '3000', thresholdMode: 1, thresholds: [[$class: 'FailedThreshold', failureNewThreshold: '', failureThreshold: '', unstableNewThreshold: '', unstableThreshold: ''], [$class: 'SkippedThreshold', failureNewThreshold: '', failureThreshold: '', unstableNewThreshold: '', unstableThreshold: '']], tools: [[$class: 'XUnitDotNetTestType', deleteOutputFiles: true, failIfNotNew: false, pattern: '*.xml', skipNoTestFiles: true, stopProcessingIfError: false]]])
+		String paths = ""
+		for (int i = 0; i < testDlls.size(); i++)
+		{
+			def testDll = testDlls[i]
+			paths += "\"$testDll.path\" "
+		}
+
+		// add platform dll to test installs
+		def wsFolder = pwd()
+		def packagesDir = "$wsFolder\\artifacts"
+		env.xunit_virto_modules_folder = packagesDir
+		paths += "\"..\\..\\..\\vc-platform\\dev\\workspace\\virtocommerce.platform.tests\\bin\\debug\\VirtoCommerce.Platform.Test.dll\""
+
+		bat "${xUnitExecutable} ${paths} -xml xUnit.Test.xml -trait \"category=ci\" -parallel none"
+		step([$class: 'XUnitPublisher', testTimeMargin: '3000', thresholdMode: 1, thresholds: [[$class: 'FailedThreshold', failureNewThreshold: '', failureThreshold: '', unstableNewThreshold: '', unstableThreshold: ''], [$class: 'SkippedThreshold', failureNewThreshold: '', failureThreshold: '', unstableNewThreshold: '', unstableThreshold: '']], tools: [[$class: 'XUnitDotNetTestType', deleteOutputFiles: true, failIfNotNew: false, pattern: '*.xml', skipNoTestFiles: true, stopProcessingIfError: false]]])
 	}
 }
