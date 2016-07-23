a9f6046b1bd6140c7262575f775d7ac6d4a09c80
Sasha <Woland2k@users.noreply.github.com>
2016-07-22 21:54:02 -0700

    Update virtoModule.groovy

--- vars/virtoModule.groovy
+++ vars/virtoModule.groovy
@@ -315,7 +315,7 @@ def runTests()
 			}
 			
 			// add platform dll to test installs
-			paths += "\"..\\..\\..\\vc-platform\\dev\\workspace\\tests\\bin\\debug\\VirtoCommerce.Platform.Test.dll\""
+			paths += "\"..\\..\\..\\vc-platform\\dev\\workspace\\virtocommerce.platform.tests\\bin\\debug\\VirtoCommerce.Platform.Test.dll\""
 			
 			bat "${xUnitExecutable} ${paths} -xml xUnit.Test.xml -trait \"category=ci\" -parallel none"
 			step([$class: 'XUnitPublisher', testTimeMargin: '3000', thresholdMode: 1, thresholds: [[$class: 'FailedThreshold', failureNewThreshold: '', failureThreshold: '', unstableNewThreshold: '', unstableThreshold: ''], [$class: 'SkippedThreshold', failureNewThreshold: '', failureThreshold: '', unstableNewThreshold: '', unstableThreshold: '']], tools: [[$class: 'XUnitDotNetTestType', deleteOutputFiles: true, failIfNotNew: false, pattern: '*.xml', skipNoTestFiles: true, stopProcessingIfError: false]]])
