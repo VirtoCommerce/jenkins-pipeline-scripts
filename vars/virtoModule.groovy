860fa5d9bc7f9224198314044f31138679aab0c1
Sasha <Woland2k@users.noreply.github.com>
2017-06-14 11:04:32 -0700

    Create virtoModule.groovy

--- vars/virtoModule.groovy
+++ vars/virtoModule.groovy
@@ -12,32 +12,7 @@ import groovy.util.*
     body()
 
     node
-    {
-		/*
-		// workaround for the issue https://issues.jenkins-ci.org/browse/JENKINS-38706
-        String path = pwd()
-        String branchName = env.BRANCH_NAME
-        if (branchName) {
-            path = path.split(Pattern.quote(File.separator))
-            def workspaceRoot = path[0..<-1].join(File.separator)
-            def currentWs = path[-1]
-            // Here is where we make branch names safe for directories -
-            // the most common bad character is '/' in 'feature/add_widget'
-            // which gets replaced with '%2f', so JOB_NAME will be
-            // ${PR}}OJECT_NAME}%2f${BRANCH_NAME}
-            String newWorkspace = env.JOB_NAME.replace('/', '-')
-            newWorkspace = newWorkspace.replace(File.separator, '-')
-            newWorkspace = newWorkspace.replace('%2f', '-')
-            newWorkspace = newWorkspace.replace('%2F', '-')
-            // Add on the '@n' suffix if it was there
-            if (currentWs =~ '@') {
-                newWorkspace = "${newWorkspace}@${currentWs.split('@')[-1]}"
-            }
-            path = "${workspaceRoot}${File.separator}${newWorkspace}"
-        }		
-		
-		ws(path) {
-			*/		
+    {	
 		try {
 			stage 'Checkout'		
 			checkout scm
