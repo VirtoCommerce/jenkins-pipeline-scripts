225cdf83463ad2d9a4716786944f13b8f0dc70e5
Alexander Siniouguine <sasha@virtoway.com>
2017-08-18 19:21:44 -0700

    update paths

--- vars/virtoPlatform.groovy
+++ vars/virtoPlatform.groovy
@@ -48,7 +48,7 @@ def call(body) {
 		    	runTests()
 			}
 			stage('Prepare Release') {
-				Utilities.createReleaseArtifact(getVersion(), webProject, zipArtifact, websiteDir)
+				jobs.scripts.Utilities.createReleaseArtifact(getVersion(), webProject, zipArtifact, websiteDir)
 			}
 
 			bat "\"${tool 'Git'}\" log -1 --pretty=%%B > LAST_COMMIT_MESSAGE"
