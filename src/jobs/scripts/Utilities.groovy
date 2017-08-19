3b0b1e3ef1d4f73f54e9b2163a085d5e3fe75f00
Alexander Siniouguine <sasha@virtoway.com>
2017-08-18 22:46:18 -0700

    updates

--- src/jobs/scripts/Utilities.groovy
+++ src/jobs/scripts/Utilities.groovy
@@ -2,10 +2,7 @@ package jobs.scripts;
 
 class Utilities {
 
-    private static String DefaultBranchOrCommitPR = '${sha1}'
-    private static String DefaultBranchOrCommitPush = '*/master'
-    private static String DefaultRefSpec = '+refs/pull/*:refs/remotes/origin/pr/*'
-    private static String DefaultMSBuild = 'MSBuild 15.0'
+    private static String DefaultSharedLibName = 'virto-shared-library'
 
     /**
      * Get the folder name for a job.
@@ -20,6 +17,6 @@ class Utilities {
     def static runSharedPS(context, scriptName)
     {
     	def wsFolder = context.pwd()
- 	    context.bat "powershell.exe -File \"${wsFolder}\\..\\workspace@libs\\virto-shared-library\\vars\\${scriptName}\""        
+ 	    context.bat "powershell.exe -File \"${wsFolder}\\..\\workspace@libs\\${DefaultSharedLibName}}\\vars\\${scriptName}\""
     }
 }
