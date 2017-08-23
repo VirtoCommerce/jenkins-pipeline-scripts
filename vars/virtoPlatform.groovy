4a6f2f4db3f90df3c8df38d77cfc883bd112b09f
Alexander Siniouguine <sasha@virtoway.com>
2017-08-23 02:48:50 -0700

    up

--- vars/virtoPlatform.groovy
+++ vars/virtoPlatform.groovy
@@ -64,9 +64,7 @@ def call(body) {
 						Packaging.startDockerTestEnvironment(this, dockerTag)
 				        
 						// now create sample data
-        				Packaging.createSampleData(this)
-						
-						Packaging.stopDockerTestEnvironment(this, dockerTag)
+        				Packaging.createSampleData(this)					
 					}
 				}
 			}
@@ -101,8 +99,9 @@ def call(body) {
 			throw any //rethrow exception to prevent the build from proceeding
 		}
 		finally {
-				step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: emailextrecipients([[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider']])])
-	    		//step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: 'dev@virtoway.com', sendToIndividuals: true])
+			Packaging.stopDockerTestEnvironment(this, dockerTag)
+			step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: emailextrecipients([[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider']])])
+	    	//step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: 'dev@virtoway.com', sendToIndividuals: true])
 		}
 	
 	  	step([$class: 'GitHubCommitStatusSetter', statusResultSource: [$class: 'ConditionalStatusResultSource', results: []]])
