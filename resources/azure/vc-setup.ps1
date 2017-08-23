93bb5ee0df399333e42ee843807db22e7f5594f5
Alexander Siniouguine <sasha@virtoway.com>
2017-08-23 02:55:20 -0700

    added module install

--- resources/azure/vc-setup.ps1
+++ resources/azure/vc-setup.ps1
@@ -7,27 +7,51 @@ Param(
         $apiurl
      )
 
-$sampleDataStateUrl = "$apiurl/api/platform/sampledata/state"
-$sampleDataImportUrl = "$apiurl/api/platform/sampledata/autoinstall"
-          
-# Initiate sample data installation
-$sampleDataImportResult = Invoke-RestMethod $sampleDataImportUrl -Method Post -ErrorAction Stop
-Write-Output "Sample data import result: $sampleDataImportResult"
+     $modulesStateUrl = "$apiurl/api/platform/modules/state"
+     $modulesInstallUrl = "$apiurl/api/platform/modules/autoinstall"
+     $sampleDataStateUrl = "$apiurl/api/platform/sampledata/state"
+     $sampleDataImportUrl = "$apiurl/api/platform/sampledata/autoinstall"
 
-# Wait until sample data have been imported
-Write-Output "Waiting for sample data import is completed"
-do
-{
-    try
-    {
-        Start-Sleep -s 5
-        $sampleDataState = Invoke-RestMethod $sampleDataStateUrl -ErrorAction Stop
-        Write-Output "Sample data state: $sampleDataState"
-    }
-    catch
-    {
-        $message = $_.Exception.Message
-        Write-Output "Error: $message"
-    }
-}
-while ($sampleDataState -ne "completed")
+     # Initiate modules installation
+     $moduleImportResult = Invoke-RestMethod $modulesInstallUrl -Method Post -ErrorAction Stop
+     Write-Output "Module install result: $moduleImportResult"
+
+     # Wait until sample data have been imported
+     Write-Output "Waiting for modules install to be completed"
+     do
+     {
+      try
+      {
+            Start-Sleep -s 5
+            $moduleState = Invoke-RestMethod $modulesStateUrl -ErrorAction Stop
+            Write-Output "Module install state: $moduleState"
+      }
+      catch
+      {
+            $message = $_.Exception.Message
+            Write-Output "Error: $message"
+      }
+     }
+     while ($moduleState -ne "completed")
+          
+      # Initiate sample data installation
+      $sampleDataImportResult = Invoke-RestMethod $sampleDataImportUrl -Method Post -ErrorAction Stop
+      Write-Output "Sample data import result: $sampleDataImportResult"
+
+      # Wait until sample data have been imported
+      Write-Output "Waiting for sample data import to be completed"
+      do
+      {
+      try
+      {
+            Start-Sleep -s 5
+            $sampleDataState = Invoke-RestMethod $sampleDataStateUrl -ErrorAction Stop
+            Write-Output "Sample data state: $sampleDataState"
+      }
+      catch
+      {
+            $message = $_.Exception.Message
+            Write-Output "Error: $message"
+      }
+      }
+      while ($sampleDataState -ne "completed")
