d0faa4532460d148194feb94ce31fe2a7ba7798d
Sasha <sasha@virtoway.com>
2017-08-23 13:01:29 -0700

    fix ps script

--- resources/azure/vc-setup.ps1
+++ resources/azure/vc-setup.ps1
@@ -59,4 +59,4 @@ Param(
             Write-Output "Error: $message"
       }
       }
-      while ($sampleDataState -ne "completed" && $cycleCount <= 24)
+      while ($sampleDataState -ne "completed" && $cycleCount -lt 24)
