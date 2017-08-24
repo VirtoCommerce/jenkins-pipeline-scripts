6f61e3a742ce6e0a0d32a875c0049b4db3f9cbb6
Alexander Siniouguine <sasha@virtoway.com>
2017-08-24 15:22:58 -0700

    add secrets

--- resources/azure/Utilities.ps1
+++ resources/azure/Utilities.ps1
@@ -2,12 +2,16 @@ Function convert-fromhex
 {
     process
     {
-        $_ -replace '^0x', '' -split "(?<=\G\w{2})(?=\w{2})" | %{ [Convert]::ToByte( $_, 16 ) }
+        $_ -replace '^0x', '' -split "(?<=\G\w{2})(?=\w{2})" | ForEach-Object { [Convert]::ToByte( $_, 16 ) }
     }
 }
 
 Function Create-Authorization
 {
+    # constants for demo sites
+    $appId = '27e0d789f12641049bd0e939185b4fd2'
+    $secret = '34f0a3c12c9dbb59b63b5fece955b7b2b9a3b20f84370cba1524dd5c53503a2e2cb733536ecf7ea1e77319a47084a3a2c9d94d36069a432ecc73b72aeba6ea78'
+
     $timestampString = [System.DateTime]::UtcNow.ToString("o", [System.Globalization.CultureInfo]::InvariantCulture)
     $hmacsha = New-Object System.Security.Cryptography.HMACSHA256
     $hmacsha.key = $secret | convert-fromhex
