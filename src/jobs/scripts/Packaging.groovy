17093ce44c6a271d711cddb290a7f07cedcc91a3
Alexander Siniouguine <sasha@virtoway.com>
2017-08-26 02:35:00 -0700

    fix build  tag

--- src/jobs/scripts/Packaging.groovy
+++ src/jobs/scripts/Packaging.groovy
@@ -53,7 +53,7 @@ class Packaging {
             // 1. stop containers
             // 2. remove instances including database
             // 3. start up new containers
-            context.withEnv(["DOCKER_TAG=${dockerTag}", "DOCKER_PLATFORM_PORT=${platformPort}", "DOCKER_STOREFRONT_PORT=${storefrontPort}", "DOCKER_SQL_PORT=${sqlPort}", "COMPOSE_PROJECT_NAME=${BUILD_TAG}" ]) {
+            context.withEnv(["DOCKER_TAG=${dockerTag}", "DOCKER_PLATFORM_PORT=${platformPort}", "DOCKER_STOREFRONT_PORT=${storefrontPort}", "DOCKER_SQL_PORT=${sqlPort}", "COMPOSE_PROJECT_NAME=${context.env.BUILD_TAG}" ]) {
                 context.bat "docker-compose stop"
                 context.bat "docker-compose rm -f"
                 context.bat "docker-compose up -d"
@@ -66,7 +66,7 @@ class Packaging {
         def composeFolder = Utilities.getComposeFolder(context)
         context.dir(composeFolder)
         {
-            context.withEnv(["DOCKER_TAG=${dockerTag}", "COMPOSE_PROJECT_NAME=${BUILD_TAG}"]) {
+            context.withEnv(["DOCKER_TAG=${dockerTag}", "COMPOSE_PROJECT_NAME=${context.env.BUILD_TAG}"]) {
                 context.bat "docker-compose stop"
             }
         }
