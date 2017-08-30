e6f3b6629d904400b5e7414e42f1b46b5d05ccc0
Alexander Siniouguine <sasha@virtoway.com>
2017-08-30 14:13:33 -0700

    draft fix

--- src/jobs/scripts/Packaging.groovy
+++ src/jobs/scripts/Packaging.groovy
@@ -263,7 +263,7 @@ class Packaging {
 		context.bat "\"${context.tool 'Git'}\" log -1 --pretty=%%B > LAST_COMMIT_MESSAGE"
 		def git_last_commit = context.readFile('LAST_COMMIT_MESSAGE')			
 
-		if (context.env.BRANCH_NAME == 'master' && git_last_commit.contains('[draft]')) {
+		if (context.env.BRANCH_NAME == 'master' && !git_last_commit.contains('[draft]')) {
 			return true
 		}
 
