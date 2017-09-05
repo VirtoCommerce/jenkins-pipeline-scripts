2c4c2cf1d822611f909cda2ecc0f8d23286c948f
Aleksandr Vishnyakov <asvishnyakov@hotmail.com>
2017-09-06 04:34:56 +0500

    virtoTheme replaced with new version

--- vars/virtoTheme.groovy
+++ vars/virtoTheme.groovy
@@ -9,17 +9,43 @@ def call(body) {
 	body.delegate = config
 	body()
     
-	node
-	{		
+	node {
+	    def storeName = config.sampleStore
 		try {
 			echo "Building branch ${env.BRANCH_NAME}"
 			Utilities.notifyBuildStatus(this, "Started")
 
-			stage('Build') {
+			def checkAndAbortBuild = false
+			stage('Checkout') {
 				timestamps { 
 					checkout scm
+				}
+				checkAndAbortBuild = Utilities.checkAndAbortBuild(this)
+				// clean folder for a release
+				if(Packaging.getShouldPublish(this) && !checkAndAbortBuild) {
+					deleteDir()
+					checkout scm
+				}
+			}
+
+			if(checkAndAbortBuild) {
+				return true
+			}
+
+			stage('Build + Analyze') {
+				timestamps { 
+					Packaging.startAnalyzer(this)
 					Packaging.runGulpBuild(this)
 				}
+			}
+
+			if (Packaging.getShouldStage(this)) {
+				stage('Stage') {
+					timestamps {
+					    def stagingName = Utilities.getStagingNameFromBranchName(this)
+						Utilities.runSharedPS(this, "resources\\azure\\VC-Theme2Azure.ps1", /-StagingName "${stagingName}" -StoreName "${storeName}"/)
+					}
+				}			
 			}
 
 			if (Packaging.getShouldPublish(this)) {
@@ -27,7 +53,7 @@ def call(body) {
 					timestamps { 
 						Packaging.publishThemePackage(this)
 					}
-				}			
+				}
 			}
 		}
 		catch (any) {
