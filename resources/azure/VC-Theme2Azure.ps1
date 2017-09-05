734b500d064e02db3bb5eeef12996931df5d8359
Aleksandr Vishnyakov <asvishnyakov@hotmail.com>
2017-09-06 03:41:27 +0500

    Completed theme deployment script

--- resources/azure/VC-Theme2Azure.ps1
+++ resources/azure/VC-Theme2Azure.ps1
@@ -1,20 +1,16 @@
 ﻿param([string] $StagingName, [string] $StoreName)
 
-$StagingName
-$StoreName
+$ErrorActionPreference = "Stop"
 
 # Get Theme Zip File
 
 $Path2Zip = Get-Childitem -Recurse -Path "${env:WORKSPACE}\artifacts\" -File -Include *.zip
 
-$Path2Zip
-
 # Unzip Theme Zip File
+
 $Path = "${env:WORKSPACE}\artifacts\" + [System.IO.Path]::GetFileNameWithoutExtension($Path2Zip)
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory($Path2Zip, $Path)
-
-$Path
 
 # Upload Theme Zip File to Azure
 
@@ -25,13 +21,12 @@ if ($StagingName -eq "dev"){
 if ($StagingName -eq "qa"){
     $ConnectionString = $ConnectionString -f ${env:AzureBlobNameQA}, ${env:AzureBlobKeyQA}
 }
-$ConnectionString
 $BlobContext = New-AzureStorageContext -ConnectionString $ConnectionString
 
-$AzureBlobName = "Themes/" + $StoreName
+$AzureBlobName = "Themes/$StoreName"
 
-Write-Host "Remove from " + $StoreName
-Remove-AzureStorageBlob -Blob $AzureBlobName -Container "cms" -Context $BlobContext
+Write-Host "Remove from $StoreName"
+Get-AzureStorageBlob -Blob ("$AzureBlobName*") -Container "cms" -Context $BlobContext  | ForEach-Object { Remove-AzureStorageBlob -Blob $_.Name -Container "cms" -Context $BlobContext } -ErrorAction Continue
 
-Write-Host "Upload to " + $StoreName
-Get-ChildItem -File -Recurse $Path | ForEach-Object { Set-AzureStorageBlobContent -File $_.FullName -Blob $AzureBlobName + "/" + (([System.Uri]($Path)).MakeRelativeUri([System.Uri]($_.FullName))).ToString() -Container "cms" -Context $BlobContext }
+Write-Host "Upload to $StoreName"
+Get-ChildItem -File -Recurse $Path | ForEach-Object { Set-AzureStorageBlobContent -File $_.FullName -Blob ("$AzureBlobName/" + (([System.Uri]("$Path/")).MakeRelativeUri([System.Uri]($_.FullName))).ToString()) -Container "cms" -Context $BlobContext }
