ecb37adb6e2a4af5083609e7420effd62749047c
Alexander Siniouguine <sasha@virtoway.com>
2017-10-11 20:13:06 -0700

    fix compilation

--- src/jobs/scripts/Packaging.groovy
+++ src/jobs/scripts/Packaging.groovy
@@ -167,7 +167,7 @@ class Packaging {
         if(context.projectType = 'NETCORE2')
         {
             context.bat "dotent restore"
-            context.bat "dontnet msbuild" \"${solution}\" /p:Configuration=Debug /p:Platform=\"Any CPU\" /m"
+            context.bat "dontnet msbuild \"${solution}\" /p:Configuration=Debug /p:Platform=\"Any CPU\" /m"
         }
         else
         {
