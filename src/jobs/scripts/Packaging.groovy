93cfa4fdf63b291d5a0d7cd949e6a25210f8a8aa
Alexander Siniouguine <sasha@virtoway.com>
2017-10-18 01:07:55 -0700

    f

--- src/jobs/scripts/Packaging.groovy
+++ src/jobs/scripts/Packaging.groovy
@@ -176,10 +176,10 @@ class Packaging {
             //context.bat "dotnet restore" // no need to run it in .net core 2.0, it should run as part of dotnet msbuild
             //context.bat "dotnet msbuild \"${solution}\" -c Debug"
             // we need to use MSBuild directly to allow sonar analysis to work
-            context.bat "
+            context.bat """
                 dotnet restore
                 \"${context.tool DefaultMSBuild}\" \"${solution}\" /p:Configuration=Debug /p:Platform=\"Any CPU\" /m"
-            "
+            """
         }
         else
         {
