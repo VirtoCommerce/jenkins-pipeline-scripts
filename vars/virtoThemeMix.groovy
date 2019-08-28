a3b04f0beefeaaf41e5959a1c64c21bd5321b733
Dennis T <facedolls@gmail.com>
2019-08-28 12:29:29 +0200

    ThemMix cleanip fix.

--- vars/virtoThemeMix.groovy
+++ vars/virtoThemeMix.groovy
@@ -74,7 +74,7 @@ def call(body) {
 			{
 				version = Utilities.getPackageVersion(this)
 			}
-			bat "rmdir .git .vs .vscode .scannerwork artifacts node_modules ng-app@tmp ng-app\\node_modules /s /q"
+			bat "rmdir .git .vs .vscode .scannerwork node_modules ng-app@tmp ng-app\\node_modules /s /q"
 			bat "del .deployment .gitignore Jenkinsfile package-lock.json deploy.cmd /s /q"
 			def zipFile = "${env.WORKSPACE}\\artifacts\\dental-theme-${version}.zip"
 			stage('Packaging')
