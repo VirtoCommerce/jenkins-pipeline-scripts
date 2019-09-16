3aff66e43149250b9cec8841d9df64337ed74198
Alexander Morogov <asmorogov@gmail.com>
2019-09-16 16:06:32 +0200

    typo

--- src/jobs/scripts/Packaging.groovy
+++ src/jobs/scripts/Packaging.groovy
@@ -236,11 +236,11 @@ class Packaging {
             def prNumber = Utilities.getPullRequestNumber(context)
             def orgName = Utilities.getOrgName(context)
             if(Utilities.isPullRequest(context)){
-                context.bat "\"${sqScannerMsBuildHome}\\SonarScanner.MSBuild.exe'\" begin /d:\"sonar.branch=${context.env.BRANCH_NAME}\" /n:\"${fullJobName}\" /k:\"${fullJobName}\" /d:sonar.github.oauth=${context.env.GITHUB_TOKEN} /d:sonar.analysis.mode=preview /d:sonar.github.pullRequest=\"${prNumber}\" /d:sonar.github.repository=${orgName}/${repoName} /d:sonar.host.url=%SONAR_HOST_URL% /d:sonar.login=%SONAR_AUTH_TOKEN% /d:sonar.cs.${coverageReportType}.reportsPaths=\"${coverageFolder}\\VisualStudio.Unit.coveragexml\""
+                context.bat "\"${sqScannerMsBuildHome}\\SonarScanner.MSBuild.exe\" begin /d:\"sonar.branch=${context.env.BRANCH_NAME}\" /n:\"${fullJobName}\" /k:\"${fullJobName}\" /d:sonar.github.oauth=${context.env.GITHUB_TOKEN} /d:sonar.analysis.mode=preview /d:sonar.github.pullRequest=\"${prNumber}\" /d:sonar.github.repository=${orgName}/${repoName} /d:sonar.host.url=%SONAR_HOST_URL% /d:sonar.login=%SONAR_AUTH_TOKEN% /d:sonar.cs.${coverageReportType}.reportsPaths=\"${coverageFolder}\\VisualStudio.Unit.coveragexml\""
             }
             else{
                 // Due to SONARMSBRU-307 value of sonar.host.url and credentials should be passed on command line
-                context.bat "\"${sqScannerMsBuildHome}\\SonarScanner.MSBuild.exe'\" begin /d:\"sonar.branch=${context.env.BRANCH_NAME}\" /n:\"${fullJobName}\" /k:\"${fullJobName}\" /d:sonar.host.url=%SONAR_HOST_URL% /d:sonar.login=%SONAR_AUTH_TOKEN% /d:sonar.cs.${coverageReportType}.reportsPaths=\"${coverageFolder}\\VisualStudio.Unit.coveragexml\""
+                context.bat "\"${sqScannerMsBuildHome}\\SonarScanner.MSBuild.exe\" begin /d:\"sonar.branch=${context.env.BRANCH_NAME}\" /n:\"${fullJobName}\" /k:\"${fullJobName}\" /d:sonar.host.url=%SONAR_HOST_URL% /d:sonar.login=%SONAR_AUTH_TOKEN% /d:sonar.cs.${coverageReportType}.reportsPaths=\"${coverageFolder}\\VisualStudio.Unit.coveragexml\""
             }
         }        
     }
@@ -260,7 +260,7 @@ class Packaging {
         def sqScannerMsBuildHome = context.tool 'Scanner for MSBuild'
         def fullJobName = Utilities.getRepoName(context)
         context.withSonarQubeEnv('VC Sonar Server') {
-            context.bat "\"${sqScannerMsBuildHome}\\SonarScanner.MSBuild.exe'\" end "
+            context.bat "\"${sqScannerMsBuildHome}\\SonarScanner.MSBuild.exe\" end "
         }        
     }
 
