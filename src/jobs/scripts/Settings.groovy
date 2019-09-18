662fff7a6e0e3bab3d671e69728e7052100e1fb2
Alexander Morogov <asmorogov@gmail.com>
2019-09-18 16:19:04 +0200

    settings class

---
+++ src/jobs/scripts/Settings.groovy
@@ -0,0 +1,33 @@
+package jobs.scripts
+
+import groovy.json.JsonSlurperClassic
+
+class Settings{
+    private Object _settings
+    private String _environment
+    private String _region
+    Settings(String json){
+        _settings = new JsonSlurperClassic().parseText(json)
+    }
+    def getAt(String item){
+        if(_region == null)
+            throw new Exception("Settings error: Region is not set")
+        if(_environment == null)
+            throw new Exception("Settings error: Environment is not set")
+        if((item == 'approvers' || item == 'approvers_e2e') & !_settings.containsKey(item))
+            return ''
+        return _settings[_region][_environment][item]
+    }
+    def setRegion(String region){
+        _region = region
+    }
+    def setEnvironment(String environment){
+        _environment = environment
+    }
+    def getRegions(){
+        return _settings.keySet() as String[]
+    }
+    def containsRegion(String region){
+        return _settings.containsKey(region)
+    }
+}
