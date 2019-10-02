1dd80729fa90897475023f97b3150c683a110903
Alexander Morogov <asmorogov@gmail.com>
2019-10-02 14:16:25 +0200

    fix

--- vars/virtoThemeMix.groovy
+++ vars/virtoThemeMix.groovy
@@ -87,19 +87,17 @@ def call(body) {
 			}
 			// bat "rmdir .git .vs .vscode .scannerwork node_modules ng-app@tmp ng-app\\node_modules /s /q"
 			// bat "del .deployment .gitignore Jenkinsfile package-lock.json deploy.cmd /s /q"
-			powershell "Remove-Item -Path .git .vs .vscode .scannerwork node_modules ng-app@tmp ng-app\\node_modules .deployment .gitignore Jenkinsfile package-lock.json deploy.cmd -Force -ErrorAction Ignore"
-			def exclude_list = "(\"artifacts\", \".git\", \".vs\", \".vscode\", \".scannerwork\", \"node_modules\", \"ng-app@tmp\", \"ng-app\\node_modules\", \".deployment\", \".gitignore\", \"Jenkinsfile\", \"package-lock.json\", \"deploy.cmd\")"
+			//powershell "Remove-Item -Path .git .vs .vscode .scannerwork node_modules ng-app@tmp ng-app\\node_modules .deployment .gitignore Jenkinsfile package-lock.json deploy.cmd -Force -ErrorAction Ignore"
+			def exclude_list = "@(\"artifacts\", \".git\", \".vs\", \".vscode\", \".scannerwork\", \"node_modules\", \"ng-app@tmp\", \"ng-app\\node_modules\", \".deployment\", \".gitignore\", \"Jenkinsfile\", \"package-lock.json\", \"deploy.cmd\")"
+			powershell "Get-ChildItem -Path ${env.WORKSPACE} -Recurse -Include $exclude_list | Remove-Item -Recurse -Force"
 			//def exclude_list = "artifacts .git .vs .vscode .scannerwork node_modules ng-app@tmp ng-app\\node_modules .deployment .gitignore Jenkinsfile package-lock.json deploy.cmd"
 			def zipFile = "${env.WORKSPACE}\\artifacts\\dental-theme-${version}.zip"
 			stage('Packaging')
 			{
 				timestamps {
-					//zip zipFile: zipFile, dir: "./"
-					powershell "New-Item -ItemType Directory -Force -Path ${env.WORKSPACE}\\artifacts"
-					powershell "Copy-Item -Path (Get-Item -Path \"${env.WORKSPACE}\\*\" -Exclude ${exclude_list}).FullName -Destination ${env.WORKSPACE}\\artifacts\\tmp -Recurse -Force"
+					zip zipFile: zipFile, dir: "./"
+					//powershell "Copy-Item -Path (Get-Item -Path \"${env.WORKSPACE}\\*\" -Exclude ${exclude_list}).FullName -Destination ${env.WORKSPACE}\\artifacts\\tmp -Recurse -Force"
 					//powershell "Copy-Item -Path .\\ -Destination ${env.WORKSPACE}\\artifacts\\tmp -Exclude ${exclude_list} -Recurse"
-					zip zipFile: zipFile, dir: "${env.WORKSPACE}\\artifacts\\tmp\\"
-					powershell "Remove-Item ${env.WORKSPACE}\\artifacts\\tmp -Recurse -Force"
 					//powershell "Get-ChildItem ./ -Directory | where { \$_.Name -notin ${exclude_list}} | Compress-Archive -DestinationPath ${zipFile} -Update"
 
 					if(params.themeResultZip != null) {
