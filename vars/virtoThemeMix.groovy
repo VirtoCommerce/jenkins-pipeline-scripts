d5ed1ca5adc9851f3ec9290c632f6430b4fe72ed
Alexander Morogov <asmorogov@gmail.com>
2019-10-02 10:09:29 +0200

    themeMix cleaning before zip

--- vars/virtoThemeMix.groovy
+++ vars/virtoThemeMix.groovy
@@ -87,7 +87,7 @@ def call(body) {
 			}
 			// bat "rmdir .git .vs .vscode .scannerwork node_modules ng-app@tmp ng-app\\node_modules /s /q"
 			// bat "del .deployment .gitignore Jenkinsfile package-lock.json deploy.cmd /s /q"
-			//powershell "Remove-Item -Path .git .vs .vscode .scannerwork node_modules ng-app@tmp ng-app\\node_modules .deployment .gitignore Jenkinsfile package-lock.json deploy.cmd -ErrorAction Ignore"
+			powershell "Remove-Item -Path .git .vs .vscode .scannerwork node_modules ng-app@tmp ng-app\\node_modules .deployment .gitignore Jenkinsfile package-lock.json deploy.cmd -Force -ErrorAction Ignore"
 			def excludes_list = "@(\"artifacts\", \".git\", \".vs\", \".vscode\", \".scannerwork\", \"node_modules\", \"ng-app@tmp\", \"ng-app\\node_modules\", \".deployment\", \".gitignore\", \"Jenkinsfile\", \"package-lock.json\", \"deploy.cmd\")"
 			def zipFile = "${env.WORKSPACE}\\artifacts\\dental-theme-${version}.zip"
 			stage('Packaging')
