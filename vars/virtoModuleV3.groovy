78a7dca69a33f041160450959feabd5ae8f8a553
Alexander Morogov <asmorogov@gmail.com>
2019-10-16 11:27:47 +0200

    modulev3 workaround

--- vars/virtoModuleV3.groovy
+++ vars/virtoModuleV3.groovy
@@ -8,28 +8,33 @@ def call(body) {
 	body()
 
     node {
-        def SETTINGS
-        def settingsFileContent
-        configFileProvider([configFile(fileId: 'shared_lib_settings', variable: 'SETTINGS_FILE')]) {
-            settingsFileContent = readFile(SETTINGS_FILE)
-        }
-        SETTINGS = new Settings(settingsFileContent)
-        SETTINGS.setRegion('platform-core')
-        SETTINGS.setEnvironment(env.BRANCH_NAME)
-        stage('Checkout'){
-            deleteDir()
-            checkout scm
-        }
+        def escapedBranch = env.BRANCH_NAME.replaceAll('/', '_')
+        def repoName = Utilities.getRepoName(this)
+        def workspace = "D:\\Buildsv3\\${repoName}\\${escapedBranch}"
+        dir(workspace){
+            def SETTINGS
+            def settingsFileContent
+            configFileProvider([configFile(fileId: 'shared_lib_settings', variable: 'SETTINGS_FILE')]) {
+                settingsFileContent = readFile(SETTINGS_FILE)
+            }
+            SETTINGS = new Settings(settingsFileContent)
+            SETTINGS.setRegion('platform-core')
+            SETTINGS.setEnvironment(env.BRANCH_NAME)
+            stage('Checkout'){
+                deleteDir()
+                checkout scm
+            }
 
-        stage('Build'){
-            bat "vc-build Compress"
-        }
+            stage('Build'){
+                bat "vc-build Compress"
+            }
 
-        stage('Unit Tests'){
-            bat "vc-build Test"
-        }   
+            stage('Unit Tests'){
+                bat "vc-build Test"
+            }   
 
-        stage('Deploy'){
+            stage('Deploy'){
+            }
         }
     }
 }
