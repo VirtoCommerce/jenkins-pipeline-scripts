211de6983452eca168eb1192cc293f252f787423
Alexander Morogov <asmorogov@gmail.com>
2019-10-17 16:36:36 +0200

    theme2azure.ps1 fix

--- resources/azure/VC-Theme2Azure.ps1
+++ resources/azure/VC-Theme2Azure.ps1
@@ -14,6 +14,7 @@ $Path2Zip = Get-Childitem -Recurse -Path "${env:WORKSPACE}\artifacts\" -File -In
 # Unzip Theme Zip File
 
 $Path = "${env:WORKSPACE}\artifacts\" + [System.IO.Path]::GetFileNameWithoutExtension($Path2Zip)
+Remove-Item $Path -Recurse -Force
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory($Path2Zip, $Path)
 
