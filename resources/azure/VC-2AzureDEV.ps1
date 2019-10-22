ee273552ddf2469404002b18b4d661661a526018
Dennis T <facedolls@gmail.com>
2019-10-22 12:02:22 +0200

    Change path.

--- resources/azure/VC-2AzureDEV.ps1
+++ resources/azure/VC-2AzureDEV.ps1
@@ -19,10 +19,11 @@ $Path2Zip = Get-Childitem -Recurse -Path "${env:WORKSPACE}\artifacts\" -File -In
 # Unzip Theme Zip File
 
 $Path = "${env:WORKSPACE}\artifacts\" + [System.IO.Path]::GetFileNameWithoutExtension($Path2Zip)
+Remove-Item $Path -Recurse -Force -ErrorAction Continue
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-#[System.IO.Compression.ZipFile]::ExtractToDirectory($Path2Zip, $Path)
+[System.IO.Compression.ZipFile]::ExtractToDirectory($Path2Zip, $Path)
 
-# Upload Theme Zip File to Azure
+# Upload Zip File to Azure
 
 $ConnectionString = "DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1};EndpointSuffix=core.windows.net"
 if ($StagingName -eq "deploy"){
@@ -30,15 +31,14 @@ if ($StagingName -eq "deploy"){
 }
 $BlobContext = New-AzureStorageContext -ConnectionString $ConnectionString
 
-$PathTo = "cms-content/Pages/vccom"
-$AzureBlobName = "$PathTo/$StoreName"
+$AzureBlobName = "Pages/vccom/$StoreName"
 
 $Now = Get-Date -format yyyyMMdd-HHmmss
 $DestContainer = $AzureBlobName + "_" + $Now
-Get-AzureStorageBlob -Blob ("$AzureBlobName*") -Container $PathTo -Context $BlobContext -Verbose | Start-AzureStorageBlobCopy -DestContainer $DestContainer -Verbose
+Get-AzureStorageBlob -Blob ("$AzureBlobName*") -Container "cms-content" -Context $BlobContext | Start-AzureStorageBlobCopy -DestContainer $DestContainer -Debug
 
 Write-Host "Remove from $StoreName"
-Get-AzureStorageBlob -Blob ("$AzureBlobName*") -Container $PathTo -Context $BlobContext  | ForEach-Object { Remove-AzureStorageBlob -Blob $_.Name -Container $PathTo -Context $BlobContext } -ErrorAction Continue
+Get-AzureStorageBlob -Blob ("$AzureBlobName*") -Container "cms-content" -Context $BlobContext  | ForEach-Object { Remove-AzureStorageBlob -Blob $_.Name -Container "cms-content" -Context $BlobContext } -ErrorAction Continue
 
 Write-Host "Upload to $StoreName"
-Get-ChildItem -File -Recurse $Path | ForEach-Object { Set-AzureStorageBlobContent -File $_.FullName -Blob ("$AzureBlobName/" + (([System.Uri]("$Path/")).MakeRelativeUri([System.Uri]($_.FullName))).ToString()) -Container $PathTo -Context $BlobContext }
+Get-ChildItem -File -Recurse $Path | ForEach-Object { Set-AzureStorageBlobContent -File $_.FullName -Blob ("$AzureBlobName/" + (([System.Uri]("$Path/")).MakeRelativeUri([System.Uri]($_.FullName))).ToString()) -Container "cms-content" -Context $BlobContext }
