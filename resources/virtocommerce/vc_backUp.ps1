eb841e3c335c34c00f1e2df1afa90443b0602fd9
Dennis Tikhonov <facedolls@gmail.com>
2019-11-17 16:39:56 +0200

    fix pathes.

--- resources/virtocommerce/vc_backUp.ps1
+++ resources/virtocommerce/vc_backUp.ps1
@@ -8,13 +8,8 @@ $destStorage="${env:destStorage}"
 $sourceSAS="${env:sourceSAS}"
 $destSAS="${env:destSAS}"
 
-Write-Host $SubscriptionID
-Write-Host $ResourceGroupName
-Write-Host $WebAppName
-Write-Host $sourceStorage
-Write-Host $destStorage
-Write-Host $sourceSAS
-Write-Host $destSAS
+Write-Host "Source storage: $sourceStorage"
+Write-Host "Dest storage: $destStorage"
 
 $ApplicationID ="${env:AzureAppID}"
 $APIKey = ConvertTo-SecureString "${env:AzureAPIKey}" -AsPlainText -Force
@@ -29,15 +24,9 @@ Stop-AzureRmWebApp -ResourceGroupName $ResourceGroupName -Name $WebAppName
 
 Start-Sleep -s 5
 
-# Getting Publish Profile
 Write-Output "Getting publishing profile for $WebAppName app"
-$tmpPublishProfile = [System.IO.Path]::GetTempFileName() + ".xml"
-
-Write-Output "Getting publishing profile for $WebAppName app"
-& "${env:Utils}\AzCopy\AzCopy" copy "https://${sourceStorage}.blob.core.windows.net/cms-content${sourceSAS}" "https://${destStorage}.blob.core.windows.net/cms-content${destSAS}"
+& "${env:Utils}\AzCopy\AzCopy" copy https://$(${sourceStorage}).blob.core.windows.net/cms-content$(${sourceSAS}) https://$(${destStorage}).blob.core.windows.net/cms-content$(${destSAS})
 Start-Sleep -s 5
 
 Write-Host "Start WebApp $WebAppName"
 Start-AzureRmWebApp -ResourceGroupName $ResourceGroupName -Name $WebAppName
-
-Remove-Item $tmpPublishProfile -Force
