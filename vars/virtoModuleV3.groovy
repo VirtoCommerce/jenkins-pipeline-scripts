19fa47ff0893609aeb95e265e170503522e733fc
Alexander Morogov <asmorogov@gmail.com>
2019-11-21 16:04:51 +0200

    fix

--- vars/virtoModuleV3.groovy
+++ vars/virtoModuleV3.groovy
@@ -56,7 +56,7 @@ def call(body) {
                         if(publishPackagesStatus != 0){
                             def nugetAlreadyExists = false
                             for(line in publishPackagesOut.trim().split("\n")){
-                                if(line.contains("error: Response status code does not indicate success: 409")){
+                                if(line.contains("409")){
                                     nugetAlreadyExists = true
                                 }
                             }
