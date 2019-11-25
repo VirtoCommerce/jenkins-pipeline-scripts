a766a17eece37660aa5de5f7f40bd60abd7cb5d0
Alexander Morogov <asmorogov@gmail.com>
2019-11-25 16:22:14 +0200

    v3 refactoring

--- vars/virtoModuleV3.groovy
+++ vars/virtoModuleV3.groovy
@@ -31,9 +31,9 @@ def call(body) {
                 stage('Build'){
                     bat "dotnet build-server shutdown"
                     withSonarQubeEnv('VC Sonar Server'){
-                        // bat "vc-build SonarQubeStart -SonarUrl ${env.SONAR_HOST_URL} -SonarAuthToken \"${env.SONAR_AUTH_TOKEN}\" "// %SONAR_HOST_URL% %SONAR_AUTH_TOKEN%
-                        // bat "vc-build SonarQubeEnd -SonarUrl ${env.SONAR_HOST_URL} -SonarAuthToken ${env.SONAR_AUTH_TOKEN}"
-                        bat "vc-build StartAnalyzer -SonarUrl ${env.SONAR_HOST_URL} -SonarAuthToken ${env.SONAR_AUTH_TOKEN}"
+                        bat "vc-build SonarQubeStart -SonarUrl ${env.SONAR_HOST_URL} -SonarAuthToken \"${env.SONAR_AUTH_TOKEN}\" "// %SONAR_HOST_URL% %SONAR_AUTH_TOKEN%
+                        bat "vc-build SonarQubeEnd -SonarUrl ${env.SONAR_HOST_URL} -SonarAuthToken ${env.SONAR_AUTH_TOKEN}"
+                        //bat "vc-build StartAnalyzer -SonarUrl ${env.SONAR_HOST_URL} -SonarAuthToken ${env.SONAR_AUTH_TOKEN}"
                     }
                 }
 
