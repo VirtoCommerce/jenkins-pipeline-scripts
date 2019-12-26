81474c04098851b8efddd7a4440695e9104f145a
Alexander Morogov <asmorogov@gmail.com>
2019-12-26 10:19:09 +0200

    platform v3 docker fix

--- vars/vcPlatformCore.groovy
+++ vars/vcPlatformCore.groovy
@@ -54,7 +54,7 @@ def call(body) {
 
 					def websitePath = Utilities.getWebPublishFolder(this, "docker")
                     def dockerImageName = "platform-core"
-                    powershell script: "Copy-Item ${workspace}\\artifacts\\publish\\* ${websitePath}\\VirtoCommerce.Platform -Recurse"
+                    powershell script: "Copy-Item ${workspace}\\artifacts\\publish\\* ${websitePath}\\VirtoCommerce.Platform -Recurse -Force"
                     powershell script: "Copy-Item ${env.WORKSPACE}@libs\\virto-shared-library\\resources\\docker.core\\windowsnano\\PlatformCore\\* ${websitePath}"
                     dir(websitePath){
                         docker.build("${dockerImageName}:${dockerTag}")
