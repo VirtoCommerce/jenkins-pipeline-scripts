79ef6f21f1a438fc748571a4df256a6d024875e1
Alexander Morogov <asmorogov@gmail.com>
2020-01-15 14:18:20 +0200

    qaenv fixed modules install script

--- resources/azure/qaenv/install-modules.ps1
+++ resources/azure/qaenv/install-modules.ps1
@@ -1,8 +1,8 @@
+#Install modules to platform container
 Param(
-    [parameter(Mandatory = $true)]
-    $AppName,
     $ApiUrl,
-    $PlatformContainer
+    $PlatformContainer,
+    $ModulesDir
 )
 . $PSScriptRoot\Utilities.ps1
 
@@ -20,35 +20,12 @@ $headers.Add("Authorization", $headerValue)
 
 $restartUrl = "$ApiUrl/api/platform/modules/restart"
 
+$checkModulesUrl = "$ApiUrl/api/platform/modules"
+
 # LOCAL PATH TO STORE ARCHIVE WITH WEBSITE CODE
 $Archive = [System.IO.Path]::GetTempFileName() + ".zip"
 
-# Credentials and URL for SOURCE website. Will download code from this site.
-# You can get credentials from the file obtained by pressing "Get publish profile" at WebApp overview page
-# Add "`" at the begging of userName from .PublishSettings file
-$sourcewebapp_username = $env:AZURE_CRED_USR
-# userPWD in .PublishSettings file
-$sourcewebapp_password = $env:AZURE_CRED_PSW
-# Convert to Base64 string for authing
-$sourcewebapp_base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $sourcewebapp_username, $sourcewebapp_password)))
-# Url for source
-$sourcewebapp_apiUrl = "https://${AppName}.scm.azurewebsites.net/api/zip/site/wwwroot/modules/"
-
-
-# DOWNLOAD WEBSITE CODE AS ARCHIVE
-Write-Output "DOWNLOAD WEBSITE CODE AS ARCHIVE"
-$WebClient = New-Object System.Net.WebClient
-$WebClient.Headers.Add('Authorization', 'Basic ' + $sourcewebapp_base64AuthInfo)
-$WebClient.Headers.Add('ContentType', 'multipart/form-data')
-try{
-    $WebClient.DownloadFile($sourcewebapp_apiUrl, $Archive)
-}
-catch{
-    Write-Output $_.Exception.Message
-    exit 1
-}
-$WebClient.Dispose()
-Remove-Variable -Name WebClient
+Compress-Archive -Path "$ModulesDir\*" -DestinationPath $Archive
 
 Write-Output "Upload modules to the container"
 docker cp $Archive ${PlatformContainer}:/vc-platform/
@@ -59,3 +36,23 @@ Remove-Item $Archive -Force
 
 Write-Output "Restarting website"
 Invoke-RestMethod "$restartUrl" -Method Post -ContentType "application/json" -Headers $headers
+
+#Check installed modules
+Write-Output "Check installed modules"
+$modules = Invoke-RestMethod $checkModulesUrl -Method Get -Headers $headers -ErrorAction Stop
+$installedModules = 0
+if ($modules.Length -le 0) {
+    Write-Output "No module's info returned"
+    exit 1
+}
+Foreach ($module in $modules) {
+    if ($module.isInstalled) {
+        $installedModules++
+    }
+    if ($module.validationErrors.Length -gt 0) {
+        Write-Output $module.id
+        Write-Output $module.validationErrors
+        exit 1
+    }
+}
+Write-Output "Modules installed: $installedModules"
