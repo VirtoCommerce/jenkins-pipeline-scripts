31c7ecc5df60b70f3105046c08ae8824b9119fe0
Alexander Morogov <asmorogov@gmail.com>
2020-02-03 10:00:39 +0200

    backup job

---
+++ backup.groovy
@@ -0,0 +1,46 @@
+def TMP_DIR
+def ZIP_NAME
+
+pipeline {
+    agent any
+
+    options
+    {
+        timestamps()
+    }
+
+    stages
+    {
+        stage("Collecting Data")
+        {
+            steps
+            {
+                script
+                {
+                    def targetFiles = []
+                    targetFiles.add("${env.JENKINS_HOME}\\org.jenkinsci.plugins.configfiles.GlobalConfigFiles.xml")
+                    def tmpDir = "${env.WORKSPACE}@tmp\\tmpDir"
+                    TMP_DIR = tmpDir
+                    dir(tmpDir)
+                    {
+                        deleteDir()
+                        pwsh "Copy-Item -Path ${targetFiles.join(',')} -Destination ${tmpDir} -Recurse -Force"
+                    }
+                    ZIP_NAME = "${env.BUILD_ID}.zip"
+                    zip zipFile: zipName, dir: tmpDir
+                }
+            }
+        }
+
+        stage("Sending Archive")
+        {
+            steps
+            {
+                script
+                {
+                    
+                }
+            }
+        }
+    }
+}
