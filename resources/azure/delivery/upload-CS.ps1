f3feeb3c93943147ead5c08f79d4cd40a6f8e744
Alexander Morogov <asmorogov@gmail.com>
2020-02-21 14:20:16 +0200

    Delivery storageAccount fix

--- resources/azure/delivery/upload-CS.ps1
+++ resources/azure/delivery/upload-CS.ps1
@@ -6,7 +6,9 @@ param(
     $WebAppName,
     $WebAppPublicName,
     $ResourceGroupName,
-    $SubscriptionID
+    $SubscriptionID,
+    $StorageAccount = "qademovc",
+    $BlobContainerName = "cms"
 )
 
 # Upload Storefront Zip File to Azure
@@ -69,14 +71,12 @@ if($StorefrontDir){
 #    Write-Output "Upload Theme"
 #    & $msdeploy -verb:sync -dest:contentPath="D:\home\site\wwwroot\wwwroot\theme",computerName=$sourcewebapp_msdeployUrl,publishSettings=$FrontendPublishProfile -source:contentPath=$ThemeDir
 #}
-$ContainerName = "cms"
 $dirpath = "Themes"
 
 
 Write-Output "AzCopy $elecPath"
-$accountname = "qademovc"
 $token = $env:AzureBlobToken
-& "${env:Utils}\AzCopy10\AzCopy" sync $ThemeDir https://$($accountname).blob.core.windows.net/$ContainerName/$($dirpath)$token --delete-destination=true #/DestKey:$accountKey /S
+& "${env:Utils}\AzCopy10\AzCopy" sync $ThemeDir https://$($StorageAccount).blob.core.windows.net/$BlobContainerName/$($dirpath)$token --delete-destination=true #/DestKey:$accountKey /S
 
 Write-Host "Start Backend $WebAppName"
 
