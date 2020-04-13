4c9b0f0e17946982880438676ed47c41de58e172
Dennis T <facedolls@gmail.com>
2020-04-13 08:16:37 +0200

    VP-1262 ARM-templates update. (#21)
    * Check project structure.
    
    * Move templates to diff folder.
    
    * Deploy script update.
    
    * Deploy script fix.
    
    * Check old path.
    
    * Change template for test delivery.
    
    * Change to Lucene.
    
    * Template update redis conn string.
    
    * Move templates to diff.folder p2.
    
    * Templates update.

--- resources/azure/arm-templates/vc-CreateInfrastructureDEV-demoVC.ps1
+++ resources/azure/arm-templates/vc-CreateInfrastructureDEV-demoVC.ps1
@@ -11,8 +11,9 @@ Select-AzureRmSubscription -SubscriptionId $SubscriptionID
 
 $DestResourceGroupName = "DEV-demoVC"
 
-$TemplateFile = "${env:WORKSPACE}\resources\azure\azuredeployDEV-demoVC.json"
-$TemplateParametersFile = "${env:WORKSPACE}\resources\azure\azuredeployDEV-demoVC_parameters.json"
+$TemplateFolder = "resources\azure"
+$TemplateFile = "${env:WORKSPACE}\$TemplateFolder\azuredeployDEV-demoVC.json"
+$TemplateParametersFile = "${env:WORKSPACE}\$TemplateFolder\azuredeployDEV-demoVC_parameters.json"
 
 New-AzureRmResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
 -ResourceGroupName $DestResourceGroupName `
