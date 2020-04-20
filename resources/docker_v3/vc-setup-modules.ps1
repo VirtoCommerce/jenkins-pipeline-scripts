81af53216509cdd4b30278632ae2e09851483a51
Alexander Morogov <asmorogov@gmail.com>
2020-04-20 08:52:05 +0200

    v3 setup-modules.ps1 retry count

--- resources/docker_v3/vc-setup-modules.ps1
+++ resources/docker_v3/vc-setup-modules.ps1
@@ -18,7 +18,7 @@ function Get-AuthToken {
     $content_type = "application/x-www-form-urlencoded"
 
     $body = @{username=$username; password=$password; grant_type=$grant_type}
-    $response = Invoke-WebRequest -Uri $appAuthUrl -Method Post -ContentType $content_type -Body $body -SkipCertificateCheck
+    $response = Invoke-RestMethod -Uri $appAuthUrl -Method Post -ContentType $content_type -Body $body -SkipCertificateCheck
     $responseContent = $response.Content | ConvertFrom-Json
     #return "$($responseContent.token_type) $($responseContent.access_token)"
     return $responseContent.access_token
@@ -34,7 +34,7 @@ $appAuthUrl = "$ApiUrl/connect/token"
 
 # Call homepage, to make sure site is compiled
 Write-Output "Call homepage, to make sure site is compiled"
-$initResult = Invoke-WebRequest $ApiUrl -UseBasicParsing -SkipCertificateCheck
+$initResult = Invoke-RestMethod $ApiUrl -UseBasicParsing -SkipCertificateCheck -RetryIntervalSec 5 -MaximumRetryCount 5
 if ($initResult.StatusCode -ne 200) {
     # throw exception when site can't be opened
     Write-Output "Can't open admin site homepage"
