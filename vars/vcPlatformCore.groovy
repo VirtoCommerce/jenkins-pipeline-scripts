0c274b31aaa1d3aa3f40f3681325de6ddbde0c85
Alexander Morogov <asmorogov@gmail.com>
2020-04-27 16:46:29 +0200

    git changelog

--- vars/vcPlatformCore.groovy
+++ vars/vcPlatformCore.groovy
@@ -48,38 +48,38 @@ def call(body) {
                     
                     checkout scm
                     
-// 					def changelog = gitChangelog returnType: 'STRING', template: '''# Changelog
+					def changelog = gitChangelog from: [type: 'REF', value: '3.0.0-rc.5.248'], to: [type: 'REF', value: '3.0.0-rc.5.250'], returnType: 'STRING', template: '''# Changelog
 
-// Changelog for {{ownerName}} {{repoName}}.
+Changelog for {{ownerName}} {{repoName}}.
 
-// {{#tags}}
-// ## {{name}}
-//  {{#issues}}
-//   {{#hasIssue}}
-//    {{#hasLink}}
-// ### {{name}} [{{issue}}]({{link}}) {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}}
-//    {{/hasLink}}
-//    {{^hasLink}}
-// ### {{name}} {{issue}} {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}}
-//    {{/hasLink}}
-//   {{/hasIssue}}
-//   {{^hasIssue}}
-// ### {{name}}
-//   {{/hasIssue}}
+{{#tags}}
+## {{name}}
+ {{#issues}}
+  {{#hasIssue}}
+   {{#hasLink}}
+### {{name}} [{{issue}}]({{link}}) {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}}
+   {{/hasLink}}
+   {{^hasLink}}
+### {{name}} {{issue}} {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}}
+   {{/hasLink}}
+  {{/hasIssue}}
+  {{^hasIssue}}
+### {{name}}
+  {{/hasIssue}}
 
-//   {{#commits}}
-// **{{{messageTitle}}}**
+  {{#commits}}
+**{{{messageTitle}}}**
 
-// {{#messageBodyItems}}
-//  * {{.}} 
-// {{/messageBodyItems}}
+{{#messageBodyItems}}
+ * {{.}} 
+{{/messageBodyItems}}
 
-// [{{hash}}](https://github.com/{{ownerName}}/{{repoName}}/commit/{{hash}}) {{authorName}} *{{commitTime}}*
+[{{hash}}](https://github.com/{{ownerName}}/{{repoName}}/commit/{{hash}}) {{authorName}} *{{commitTime}}*
 
-//   {{/commits}}
+  {{/commits}}
 
-//  {{/issues}}
-// {{/tags}}'''
+ {{/issues}}
+{{/tags}}'''
                     //echo changelog
                 }
 
