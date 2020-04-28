e554437fc774495204c8c758614ba0982a05f13d
Alexander Morogov <asmorogov@gmail.com>
2020-04-28 15:19:24 +0200

    theme install v3 fix

--- resources/docker_v3/vc-theme-install.ps1
+++ resources/docker_v3/vc-theme-install.ps1
@@ -8,4 +8,4 @@ Param(
 Write-Output $StorefrontContainer
 Write-Output $ThemePath
 Write-Output "docker cp"
-docker cp $ThemePath ${StorefrontContainer}:/vc-storefront/wwwroot/Themes
+docker cp $ThemePath ${StorefrontContainer}:/vc-storefront/wwwroot/cms-content/Themes
