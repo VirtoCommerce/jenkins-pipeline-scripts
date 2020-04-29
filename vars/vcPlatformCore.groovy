43f1fbaa4f576729553d70fe314e69b4e012d183
Alexander Morogov <asmorogov@gmail.com>
2020-04-29 15:55:33 +0200

    disabled changelog

--- vars/vcPlatformCore.groovy
+++ vars/vcPlatformCore.groovy
@@ -48,44 +48,44 @@ def call(body) {
                     
                     checkout scm
                     
-                    if(env.BRANCH_NAME == 'release/3.0.0')
-                    {
-                        def commitId = pwsh(returnStdout: true, script: 'git rev-parse HEAD').trim()
-                        def prevCommitId = pwsh(returnStdout: true, script: 'git rev-parse HEAD^1').trim()
-					def changelog = gitChangelog from: [type: 'REF', value: "3.0.0-rc.5.248"], to: [type: 'REF', value: "3.0.0-rc.5.250"], returnType: 'STRING', template: '''# Changelog
+//                     if(env.BRANCH_NAME == 'release/3.0.0')
+//                     {
+//                         def commitId = pwsh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+//                         def prevCommitId = pwsh(returnStdout: true, script: 'git rev-parse HEAD^1').trim()
+// 					def changelog = gitChangelog from: [type: 'REF', value: "3.0.0-rc.5.248"], to: [type: 'REF', value: "3.0.0-rc.5.250"], returnType: 'STRING', template: '''# Changelog
 
-Changelog for {{ownerName}} {{repoName}}.
+// Changelog for {{ownerName}} {{repoName}}.
 
-{{#tags}}
-## {{name}}
- {{#issues}}
-  {{#hasIssue}}
-   {{#hasLink}}
-### {{name}} [{{issue}}]({{link}}) {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}}
-   {{/hasLink}}
-   {{^hasLink}}
-### {{name}} {{issue}} {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}}
-   {{/hasLink}}
-  {{/hasIssue}}
-  {{^hasIssue}}
-### {{name}}
-  {{/hasIssue}}
+// {{#tags}}
+// ## {{name}}
+//  {{#issues}}
+//   {{#hasIssue}}
+//    {{#hasLink}}
+// ### {{name}} [{{issue}}]({{link}}) {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}}
+//    {{/hasLink}}
+//    {{^hasLink}}
+// ### {{name}} {{issue}} {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}}
+//    {{/hasLink}}
+//   {{/hasIssue}}
+//   {{^hasIssue}}
+// ### {{name}}
+//   {{/hasIssue}}
 
-  {{#commits}}
-**{{{messageTitle}}}**
+//   {{#commits}}
+// **{{{messageTitle}}}**
 
-{{#messageBodyItems}}
- * {{.}} 
-{{/messageBodyItems}}
+// {{#messageBodyItems}}
+//  * {{.}} 
+// {{/messageBodyItems}}
 
-[{{hash}}](https://github.com/{{ownerName}}/{{repoName}}/commit/{{hash}}) {{authorName}} *{{commitTime}}*
+// [{{hash}}](https://github.com/{{ownerName}}/{{repoName}}/commit/{{hash}}) {{authorName}} *{{commitTime}}*
 
-  {{/commits}}
+//   {{/commits}}
 
- {{/issues}}
-{{/tags}}'''
-                    echo changelog
-                    }
+//  {{/issues}}
+// {{/tags}}'''
+//                     echo changelog
+//                     }
                 }
 
                 if(!Utilities.areThereCodeChanges(this))
