b20bc22446cb62ca0ea0d0cc078e03d09abce5c6
Alexander Morogov <asmorogov@gmail.com>
2020-05-08 15:29:38 +0200

    release notes test

--- vars/vcPlatformCore.groovy
+++ vars/vcPlatformCore.groovy
@@ -53,7 +53,8 @@ def call(body) {
                     try
                     {
                         def release = GithubRelease.getLatestGithubReleaseRegexp(this, Utilities.getOrgName(this), Utilities.getRepoName(this), /\d\.\d\.\d[\s]{0,1}[\w]*/, true)
-                        pwsh "git log --pretty=format:\"%s\" --since=\"${release.published_at}\""
+                        def releaseNotes = Utilities.getReleaseNotesFromCommits(this, release.published_at)
+                        echo releaseNotes
                     }
                     catch(any)
                     {
