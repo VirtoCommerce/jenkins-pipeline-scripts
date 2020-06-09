c6504ee258059be036b6d823edc2cdfbe6f5a8f3
Alexander Morogov <asmorogov@gmail.com>
2020-06-09 09:09:03 +0200

    fix

--- resources/docker_v3/setup-modulesFromDir.ps1
+++ resources/docker_v3/setup-modulesFromDir.ps1
@@ -7,7 +7,7 @@ Write-Output "Stop Container ${PlatformContainer}"
 docker stop $PlatformContainer
 
 Write-Output "Upload modules to the container"
-docker cp ${ModulesDir}/. ${PlatformContainer}:/vc-platform/Modules
+docker cp ${ModulesDir}/. ${PlatformContainer}:/opt/vc-platform/Modules
 
 Write-Output "Start Container"
 docker start $PlatformContainer
