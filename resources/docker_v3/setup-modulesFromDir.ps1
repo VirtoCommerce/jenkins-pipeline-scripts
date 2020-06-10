0f25503db24e64c9aceef5f641f7dd6950ce5b4c
Alexander Morogov <asmorogov@gmail.com>
2020-06-10 11:57:14 +0200

    setup modules crossplatform fixes

--- resources/docker_v3/setup-modulesFromDir.ps1
+++ resources/docker_v3/setup-modulesFromDir.ps1
@@ -6,8 +6,14 @@ Param(
 Write-Output "Stop Container ${PlatformContainer}"
 docker stop $PlatformContainer
 
+$containerDest = "/vc-platform/Modules"
+if($IsLinux)
+{
+    Write-Output "Linux"
+    $containerDest = "/opt/vc-platform/Modules"
+}
 Write-Output "Upload modules to the container"
-docker cp ${ModulesDir}/. ${PlatformContainer}:/opt/vc-platform/Modules
+docker cp ${ModulesDir}/. ${PlatformContainer}:${containerDest}
 
 Write-Output "Start Container"
 docker start $PlatformContainer
