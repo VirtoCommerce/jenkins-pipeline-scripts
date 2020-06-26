ab4fa5d301c92145cdeb02ca0557fd4c8bb334dd
Alexander Morogov <asmorogov@gmail.com>
2020-06-26 16:46:33 +0200

    fixed swagger validation in Delivery pipeline

---
+++ resources/docker_v3/validate-swagger.ps1
@@ -0,0 +1,31 @@
+Param(
+    [parameter(Mandatory = $true)]
+    $ApiUrl,
+    [parameter(Mandatory = $true)]
+    $OutFile,
+    $SwaggerValidatorUrl = "http://validator.swagger.io/validator/debug"
+)
+
+$swaggerApi = "$ApiUrl/docs/PlatformUI/swagger.json"
+
+Write-Output "OutFile: $OutFile"
+Invoke-WebRequest -Uri $swaggerApi -Method Get -OutFile $OutFile -SkipCertificateCheck -MaximumRetryCount 5 -RetryIntervalSec 5
+
+$swaggerSchemaPath = $OutFile
+$headers = @{}
+$headers.Add("accept", "application/json")
+$schemaContent = Get-Content -Path $swaggerSchemaPath
+$result = Invoke-RestMethod -Uri $SwaggerValidatorUrl -Headers $headers -Body $schemaContent -ContentType "application/json" -Method Post -Debug -Verbose
+$errors = $false
+foreach ($validation in $result.schemaValidationMessages)
+{
+    Write-Output $validation
+    if($validation.level -eq "error")
+    {
+        $errors = $false
+    }
+}
+if($errors)
+{
+    exit 1
+}
