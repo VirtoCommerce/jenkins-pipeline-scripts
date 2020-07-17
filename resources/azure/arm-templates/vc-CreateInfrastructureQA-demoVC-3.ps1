0fb758682af576b9f88a5ad57b0e1780f14f757b
Dennis T <facedolls@gmail.com>
2020-07-17 10:57:38 +0200

    Update QA-DemoVC-3 arm.

---
+++ resources/azure/arm-templates/vc-CreateInfrastructureQA-demoVC-3.ps1
@@ -0,0 +1,25 @@
+$ApplicationID ="${env:AzureAppID}"
+$APIKey = ConvertTo-SecureString "${env:AzureAPIKey}" -AsPlainText -Force
+$psCred = New-Object System.Management.Automation.PSCredential($ApplicationID, $APIKey)
+$TenantID = "${env:AzureTenantID}"
+$SubscriptionID = "973d0b8c-44bf-438d-a4b7-1c4162d3ccba"
+
+Write-Host "Infrastructure Check and Deploy Started"
+
+Add-AzureRmAccount -Credential $psCred -TenantId $TenantID -ServicePrincipal
+Select-AzureRmSubscription -SubscriptionId $SubscriptionID
+
+$DestResourceGroupName = "QA-demoVC-3"
+
+$TemplateFolder = "resources\azure"
+$TemplateFile = "${env:WORKSPACE}\$TemplateFolder\azuredeployQA-demoVC-3.json"
+$TemplateParametersFile = "${env:WORKSPACE}\$TemplateFolder\azuredeployQA-demoVC-3_parameters.json"
+
+New-AzureRmResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
+-ResourceGroupName $DestResourceGroupName `
+-TemplateFile $TemplateFile `
+-TemplateParameterFile $TemplateParametersFile `
+-Force -Verbose `
+-ErrorVariable ErrorMessages
+
+Write-Host "Infrastructure Check and Deploy Finished"
